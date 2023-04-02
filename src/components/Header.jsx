6dba4163405fbf391a617ce1292efac372fb178f
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-04-02 01:01:50 -0400

    Crea container en App para cargar más Cards ordenadas, cambia fondo de Header a Alert

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
+import Badge from 'react-bootstrap/Badge';
+
 const CompHeader = (props) => {
-    return (<h1 className="text-center" >{props.title}</h1>);
+    return (
+        <h1 className="text-center --bg-primary" >
+            <Badge pill bg='warning'>{props.title}</Badge>
+        </h1>
+    );
 }
 
 export default CompHeader;
