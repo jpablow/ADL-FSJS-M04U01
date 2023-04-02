9639abefd53bf3b46465fd5522d0851233923329
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-04-01 23:17:33 -0400

    Crea el footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import Badge from 'react-bootstrap/Badge';
+
+const CompFooter = () => {
+    return (
+        <div>
+            <Badge pill bg="warning">Recuerda investigar cómo debes cuidar tus nuevas plantas</Badge>{' '}
+        </div>
+    );
+}
+
+export default CompFooter;
