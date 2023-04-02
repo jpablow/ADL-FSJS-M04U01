1d6ca6b289494966186a50da00761a6fb94a657a
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-04-01 23:47:55 -0300

    Agrega enlace a boton mas info de card

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <CompHeader title='Galería de Imágenes con React' />
-      <CompCard url='https://www.picturethisai.com/image-handle/website_cmsname/image/1080/153726684976119827.jpeg?x-oss-process=image/format,webp/resize,s_422&v=1.0' title='Stromanthe Thalia' text='El estromante es una especie adoptada por la decoración de interiores para ser cultivada en maceta. Su aspecto más decorativo lo aporta la combinación tan llamativa de colores que presentan sus hojas: verde, rosa y crema con el envés rojizo.' />
+      <CompCard imgUrl='https://www.picturethisai.com/image-handle/website_cmsname/image/1080/153726684976119827.jpeg?x-oss-process=image/format,webp/resize,s_422&v=1.0' title='Stromanthe Thalia' text='El estromante es una especie adoptada por la decoración de interiores para ser cultivada en maceta. Su aspecto más decorativo lo aporta la combinación tan llamativa de colores que presentan sus hojas: verde, rosa y crema con el envés rojizo.' webUrl='https://www.picturethisai.com/es/wiki/Stromanthe_thalia.html' />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,11 @@ import Card from 'react-bootstrap/Card';
 const CompCard = (props) => {
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={props.url} />
+            <Card.Img variant="top" src={props.imgUrl} />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text>{props.text}</Card.Text>
-                <Button variant="info">+ Info</Button>
+                <a href={props.webUrl}><Button variant="info">+ Info</Button></a>
             </Card.Body>
         </Card>
     );
